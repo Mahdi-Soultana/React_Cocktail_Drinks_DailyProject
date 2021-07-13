0a74d967751ed9db79f3d886d4e5d062c9b4c4d3
Mahdi <67097548+Mahdi-Soultana@users.noreply.github.com>
2021-07-13 12:35:22 -0800

    Update CocktailApp.js

--- src/components/CocktailApp/CocktailApp.js
+++ src/components/CocktailApp/CocktailApp.js
@@ -2,7 +2,7 @@ import Layout from "./Layout/Layout";
 import CocktailList from "./CocktailList/CocktailList";
 import SingleCocktail from "./pages/singleCocktail/singleCocktail";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import About from "./pages/About/About";
+import About from "./pages/about/About";
 import Error from "./pages/Error/Error";
 
 const index = () => {
