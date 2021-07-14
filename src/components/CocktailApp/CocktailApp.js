a0a39250d307b7ec99230605d4df25eefc28d202
Mahdi-Soultana <soultana.mahdi24@gmail.com>
2021-07-13 20:28:52 -0800

    fixed

--- src/components/CocktailApp/CocktailApp.js
+++ src/components/CocktailApp/CocktailApp.js
@@ -1,9 +1,9 @@
-import Layout from "./Layout/Layout.js";
+import Layout from "./Layout/Layout";
 import CocktailList from "./CocktailList/CocktailList";
 import SingleCocktail from "./pages/singleCocktail/singleCocktail";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import About from "./pages/about/About.js";
-import Error from "./pages/Error/Error.js";
+import About from "./pages/about/About";
+import Error from "./pages/Error/Error";
 
 const index = () => {
   return (
